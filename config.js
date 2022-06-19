9ffcf46e3676d59b71141889e72197647043e521
blockia-ir <info@blockia.ir>
2022-06-19 00:07:57 -0700

    Public

---
+++ config.js
@@ -0,0 +1,26 @@
+// Blockia Desktop 1.0
+// Hossein Pira
+// blockia.ir
+
+const config = {
+    // Main Application URL
+    'websiteUrl' : 'https://blockia.ir/admin',
+
+    // Application Name
+    'appName' : 'Blockia',
+
+    // Application window width and height
+    'width' : 1280,
+    'height' : 800,
+    'minWidth' : 1280,
+    'minHeight' : 800,
+
+    // Hide elements by ID
+    'hideElementsId' : [],
+
+    // Hide elements by Class
+    'hideElementsClass' : [],
+
+}
+
+module.exports = config
---
+++ right-menu-config.js
@@ -0,0 +1,20 @@
+// Blockia Desktop 1.0
+// Hossein Pira
+// blockia.ir
+
+const { ipcMain } = require('electron');
+
+module.exports = [
+    {role : 'reload'},
+    {role : 'cut'},
+    {role : 'copy'},
+    {role : 'paste'},
+    {role : 'redo'},
+    { type: 'separator' },
+    {label : 'Print', click: printPage},
+]
+
+// Print page method
+function printPage() {
+    ipcMain.emit('printPage')
+}
